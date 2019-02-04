82d5003b7a54060ec55fe6dda5c5c3f4e10e0cc7
Sanjul <sanjul.abhishek@gmail.com>
2019-02-04 09:02:12 +0800

    demo

---
+++ src/main/java/iot/demo/Application.java
@@ -0,0 +1,22 @@
+package iot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@RequestMapping("/rest")
+	public String home() {
+		return "Hello Docker ";
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Starting spring boot app");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
